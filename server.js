44acf1c3d9bbfdbc26f4810bdf115d18c53b7f0e
Vasyl Yurchenko <yurchenko.v200387@gmail.com>
2023-12-14 17:02:52 +0200

    atest-2

--- server.js
+++ server.js
@@ -9,26 +9,44 @@ app.use(express.json());
 
 app.use(cors({ origin: '*' }));
 
-const apiKey = '64964a77aa3e32bf787a793f247b9dfa';
-
 app.get('/', (req, res) => {
-    res.status(200).json({status: 200, message:"Сервер працює"});
+    res.status(200).json({status: 200, message:"Server is working!"});
 });
 
-app.get('/weather/:city', async (req, res)=> {
-    try {
-        const response = await axios.get('https://api.openweathermap.org/data/2.5/weather?q=' + req.params.city + '&appid=' + apiKey);
-        const data = response.data;
+async function getBinanceData() {
+    const usdToUahExchangeRate = 37.5;
 
-        if (response.status === 200) {
-            console.log(req.params.city);
-            res.status(200).json({ status: 200, data: data });
+    try {
+        const exchangeInfoResponse = await axios.get('https://api.binance.com/api/v3/exchangeInfo');
+        const tickerResponse = await axios.get('https://api.binance.com/api/v3/ticker/price');
+
+        const symbols = exchangeInfoResponse.data.symbols.filter(symbol => symbol.quoteAsset === "USDT").map(symbol => symbol.symbol);
+
+        const filteredPrices = tickerResponse.data
+        .filter(item => symbols.includes(item.symbol))
+        .map(item => {
+            return {
+            symbol: item.symbol,
+            priceInUSD: parseFloat(item.price),
+            priceInUAH: parseFloat(item.price) * usdToUahExchangeRate,
+            };
+        });
+        
+        return filteredPrices;
+        } catch (error) {
+            console.error('Error fetching data from Binance API:', error.message);
+            throw error;
         }
-    } catch (error) {
-        console.error(error.response.status);
-        res.status(404).json({ status: 404, message: error.response.statusText});
-    }
-    
+}
+
+app.get('/getBinancePricesUSDT', async (req, res) => {
+    try {
+        const binanceData = await getBinanceData();
+        res.json(binanceData);
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ error: 'Internal Server Error' });
+        }
 });
 
 app.listen(PORT, () => {
